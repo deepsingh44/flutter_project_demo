7c67f6e77fd176c756fea6a1f311052011ef60a2
deepsingh44 <deepsingh44@gmail.com>
2020-12-25 12:03:51 +0530

    Firebase realtimedatabase CRUD done.

--- lib/firebasedemo/firebasedatabase.dart
+++ lib/firebasedemo/firebasedatabase.dart
@@ -1,14 +1,21 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 class FirebaseDemo extends StatefulWidget {
+  Container mycontainer = Container();
+
   @override
   _FirebaseDemoState createState() => _FirebaseDemoState();
 }
 
 class _FirebaseDemoState extends State<FirebaseDemo> {
   final databaseReference = FirebaseDatabase.instance.reference();
-  String name, email, mobile;
+  final name = TextEditingController();
+  final email = TextEditingController();
+  final mobile = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  String key="";
 
   @override
   Widget build(BuildContext context) {
@@ -17,152 +24,130 @@ class _FirebaseDemoState extends State<FirebaseDemo> {
         title: Text("Firebase CRUD"),
       ),
       body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-                child: TextFormField(
-                  onChanged: (value) => name = value,
-                  validator: (value) =>
-                      (value.isEmpty) ? "Please enter name" : null,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.drive_file_rename_outline),
-                    labelText: "Name Here",
-                    border: OutlineInputBorder(
-                      gapPadding: 2,
+        child: Form(
+          key: _formKey,
+          child: Container(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: TextFormField(
+                    controller: name,
+                    //onChanged: (value) => name.text=value,
+                    validator: (value) =>
+                    (value.isEmpty) ? "Please enter name" : null,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.drive_file_rename_outline),
+                      labelText: "Name Here",
+                      border: OutlineInputBorder(
+                        gapPadding: 2,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-                child: TextFormField(
-                  onChanged: (value) => email = value,
-                  validator: (value) =>
-                      (value.isEmpty) ? "Please enter email" : null,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email),
-                    labelText: "Email Here",
-                    border: OutlineInputBorder(
-                      gapPadding: 2,
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: TextFormField(
+                    controller: email,
+                    /*onChanged: (value) {
+                      setState(() {
+                        email.text = value;
+                      });
+                    },*/
+                    validator: (value) =>
+                    (value.isEmpty) ? "Please enter email" : null,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      labelText: "Email Here",
+                      border: OutlineInputBorder(
+                        gapPadding: 2,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-                child: TextFormField(
-                  onChanged: (value) => mobile = value,
-                  validator: (value) =>
-                      (value.isEmpty) ? "Please enter mobile" : null,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.call),
-                    labelText: "Mobile Here",
-                    border: OutlineInputBorder(
-                      gapPadding: 2,
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: TextFormField(
+                    controller: mobile,
+                    /*onChanged: (value) {
+                      setState(() {
+                        mobile.text = value;
+                      });
+                    },*/
+                    validator: (value) =>
+                    (value.isEmpty) ? "Please enter mobile" : null,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.call),
+                      labelText: "Mobile Here",
+                      border: OutlineInputBorder(
+                        gapPadding: 2,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-                width: MediaQuery.of(context).size.width,
-                child: ElevatedButton(
-                  child: Text("Add Me"),
-                  onPressed: () {
-                    insert(Student(name: name, email: email, mobile: mobile));
-                  },
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  left: 20,
-                  right: 20,
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
+                  child: ElevatedButton(
+                    child: Text("Add Me"),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        insert(Student(
+                            name: name.text, email: email.text, mobile: mobile.text, key: ""));
+                        _formKey.currentState.reset();
+                      }
+                    },
+                  ),
                 ),
-                width: MediaQuery.of(context).size.width,
-                child: ElevatedButton(
-                  child: Text("Fetch All"),
-                  onPressed: () {
-                    fetchAll();
-                  },
+                Container(
+                  margin: EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                  ),
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
+                  child: ElevatedButton(
+                    child: Text("Fetch All"),
+                    onPressed: () {
+                      fetchAllInWidget().then((value) {
+                        widget.mycontainer = value;
+                      });
+                    },
+                  ),
                 ),
-              ),
-              Container(
-                child: FutureBuilder(
-                  future: fetchAll(),
-                  builder: (context, snapshot) {
-                    //Map<dynamic, dynamic> values = snapshot.data;
-                    if (snapshot.hasData) {
-                      print(snapshot.data.length);
-                      return ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (context, index) {
-                          Student student = snapshot.data[index];
-                          return Container(
-                            margin:
-                                EdgeInsets.only(left: 10, right: 10, top: 5),
-                            padding: EdgeInsets.all(1),
-                            decoration: BoxDecoration(
-                              //border: Border.all(width: 1,color: Colors.red),
-                              color: Colors.blueGrey,
-                              gradient: LinearGradient(
-                                colors: [Colors.red, Colors.green],
-                              ),
-                            ),
-                            child: ListTile(
-                              title: Text(
-                                student.name,
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              subtitle: Text(
-                                student.email,
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              leading: Icon(
-                                Icons.stacked_bar_chart,
-                                color: Colors.white,
-                                size: 45,
-                              ),
-                              trailing: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  IconButton(
-                                    icon: Icon(
-                                      Icons.edit,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  IconButton(
-                                    icon: Icon(
-                                      Icons.delete,
-                                      color: Colors.red,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            /*Text(student.name),
-                                Text(student.email),
-                                Text(student.mobile),*/
-                          );
-                        },
-                      );
-                    } else {
-                      return CircularProgressIndicator(
-                        strokeWidth: 2,
-                        backgroundColor: Colors.blue,
-                      );
-                    }
-                  },
+                Container(
+                  margin: EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                  ),
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
+                  child: ElevatedButton(
+                    child: Text("Update"),
+                    onPressed: () {
+                      setState(() {
+                        databaseReference.child("student").child(key).update(Student(name: name.text,email: email.text,mobile: mobile.text).toMap()).then((value) => {
+                          print("update successfully"),
+                        });
+                      });
+                    },
+                  ),
                 ),
-              ),
-            ],
+                Container(
+                  child: widget.mycontainer,
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -177,46 +162,124 @@ class _FirebaseDemoState extends State<FirebaseDemo> {
           .push()
           .set(student.toMap())
           .whenComplete(
-            () => () {
-              print("Successfully Added");
-            },
-          );
+            () =>
+            () {
+          print("Successfully Added");
+        },
+      );
+    });
+  }
+
+  void fetchAllDatabase() {
+    databaseReference
+        .child("student")
+        .onValue
+        .listen((event) {
+      var snap = event.snapshot;
+      setState(() {
+        print(snap.value.toString());
+      });
     });
   }
 
   Future<List<Student>> fetchAll() async {
     List<Student> students = [];
-    databaseReference.child("student").once().then((value) => {
-          print(value.value is Map),
-          //values = value.value,
-          //values.forEach((key, value) { value.from})
-          for (var val in value.value.entries)
-            {
-              students.add(Student(
-                  name: val.value["name"],
-                  email: val.value["email"],
-                  mobile: val.value["mobile"])),
-              /* print(val.value["name"] +
-                  "\t" +
-                  val.value["email"] +
-                  "\t" +
-                  val.value["mobile"]),*/
-            }
-        });
+
+    databaseReference.child("student").once().then((value) =>
+    {
+      //print(value.key),
+      for (var val in value.value.entries)
+        {
+          students.add(
+            Student(
+              name: val.value["name"],
+              email: val.value["email"],
+              mobile: val.value["mobile"],
+              key: val.key,
+            ),
+          ),
+        }
+    });
+    print("fetching completed...");
+    //fetchAllDatabase();
     return students;
+  }
+
+  Future<Widget> fetchAllInWidget() async {
+    return Container(
+      child: FirebaseAnimatedList(
+          shrinkWrap: true,
+          padding: new EdgeInsets.all(8.0),
+          reverse: false,
+          //duration: Duration(seconds: 5),
+          query: databaseReference.child("student").orderByValue(),
+          itemBuilder: (context, snapshot, animation, index) {
+            return Card(
+              child: ListTile(
+                title: Text(snapshot.value["name"]),
+                subtitle: Text(snapshot.value["email"]),
+                trailing: Container(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.edit,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            key=snapshot.key;
+                            name.text = snapshot.value["name"].toString();
+                            email.text = snapshot.value["email"].toString();
+                            mobile.text = snapshot.value["mobile"].toString();
+                          });
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.delete,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            databaseReference
+                                .child("student")
+                                .child(snapshot.key)
+                                .remove()
+                                .then((value) =>
+                            {
+                              print("deleted.."),
+                            });
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+
+  Future delete(Student student) async {
+    print(student.name);
+    print(student.key);
+    await databaseReference.child(student.key).remove();
   }
 }
 
 class Student {
-  String name, email, mobile;
+  String name, email, mobile, key;
 
-  Student({this.name, this.email, this.mobile});
+  Student({this.name, this.email, this.mobile, this.key});
 
   Map<String, dynamic> toMap() {
     return {
       'name': name,
       'email': email,
       "mobile": mobile,
+      "key": key,
     };
   }
 
@@ -224,5 +287,6 @@ class Student {
     this.name = obj['name'];
     this.email = obj['email'];
     this.mobile = obj['mobile'];
+    this.key = obj["key"];
   }
 }
